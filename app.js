7618843e4b198d257775918d539b4c1ea9cc88b0
dandnirv7 <dandnirv7@gmail.com>
2023-05-11 19:08:59 +0700

    refactor biodataRouter ke file terpisah

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const DB_PORT = process.env.DB_PORT || 8001;
-const route = require("./biodataRoutes");
+const port = process.env.SERVER_PORT || 3000;
+const biodataRouter = require("./biodataRoutes");
 
-app.use("/", route);
-
-app.get("/biodata", (req, res) => {
-  const nama = req.query.nama;
-  const tempatLahir = req.query["tempat-lahir"];
-  const tanggalLahir = req.query["tanggal-lahir"];
-  const alamat = req.query.alamat;
-
-  const biodata = {
-    nama: nama,
-    "tempat-lahir": tempatLahir,
-    "tanggal-lahir": tanggalLahir,
-    alamat: alamat,
-  };
-
-  res.send(biodata);
-});
+app.use("/", biodataRouter);
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/biodata", (req, res) => {
-  const nama = req.body.nama;
-  const tempatLahir = req.body["tempat-lahir"];
-  const tanggalLahir = req.body["tanggal-lahir"];
-  const alamat = req.body.alamat;
-
-  const biodata = {
-    nama: nama,
-    "tempat-lahir": tempatLahir,
-    "tanggal-lahir": tanggalLahir,
-    alamat: alamat,
-  };
-
-  res.send(biodata);
-});
-
-app.listen(DB_PORT, () => {
-  console.log(`Server listening on port: ${DB_PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 });
