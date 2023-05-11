ec7efa28c5b7b06f34e5542e803c44847e3d323c
dandnirv7 <dandnirv7@gmail.com>
2023-05-11 19:07:18 +0700

    refactor routes in biodataRoutes.js

--- biodataRoutes.js
+++ biodataRoutes.js
@@ -1,15 +1,47 @@
 const express = require("express");
-const biodataRouter = express.Router();
+const router = express.Router();
 
-biodataRouter.use(
+router.use(
   (timeLog = (req, res, next) => {
     console.log("Requested URI Path: ", req.url);
     next();
   })
 );
 
-biodataRouter.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Biodata Home Page");
 });
 
-module.exports = biodataRouter;
+router.get("/biodata", (req, res) => {
+  const nama = req.query.nama;
+  const tempatLahir = req.query["tempat-lahir"];
+  const tanggalLahir = req.query["tanggal-lahir"];
+  const alamat = req.query.alamat;
+
+  const biodata = {
+    nama: nama,
+    "tempat-lahir": tempatLahir,
+    "tanggal-lahir": tanggalLahir,
+    alamat: alamat,
+  };
+
+  res.send(biodata);
+});
+
+router.post("/biodata", (req, res) => {
+  const nama = req.body.nama;
+  const tempatLahir = req.body["tempat-lahir"];
+  const tanggalLahir = req.body["tanggal-lahir"];
+  const alamat = req.body.alamat;
+
+  const biodata = {
+    nama: nama,
+    "tempat-lahir": tempatLahir,
+    "tanggal-lahir": tanggalLahir,
+    alamat: alamat,
+  };
+
+  res.send(biodata);
+});
+
+module.exports = router;
